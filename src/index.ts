818da25d613d1bd8dbd4d81a4dec62580bee8893
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-12-09 11:04:12 +0100

    testing action

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    console.log("Hello World!");
-    
-    // const release: string = core.getInput("release");
-    // console.log(`Release: ${release}`);
+    const release: string = core.getInput("release");
+    console.log(`Release: ${release}`);
   } catch (error) {
     core.setFailed(error.message);
   }
