5bbfe76e81878f3392342e95effc594d7885d6fd
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-12-11 13:33:08 +0100

    testing

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,6 @@ if (!process.env.GITHUB_TOKEN) {
   throw new Error("GITHUB_TOKEN not set");
 }
 
-if (!process.env.VSCODE_ARCH) {
-  throw new Error("VSCODE_ARCH not set");
-}
-
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN as string;
 export const VSCODE_ARCH = process.env.VSCODE_ARCH as "x64" | "arm64" | "ia32";
 export const RUNNER_OS = process.env.RUNNER_OS as "macOS" | "Linux" | "Windows";
